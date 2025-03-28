9d60302b3297760bfa4f0741114a11640005e264
허승빈 <qmfhfl02@gmail.com>
2025-03-28 14:31:47 +0900

    뷰포트 관리 생성 코드 주석처리

--- Week04/Engine/Source/Runtime/Engine/World.cpp
+++ Week04/Engine/Source/Runtime/Engine/World.cpp
@@ -15,13 +15,15 @@ void UWorld::Initialize()
     // TODO: Load Scene
     CreateBaseObject();
     //SpawnObject(OBJ_CUBE);
-    FManagerOBJ::CreateStaticMesh("Assets/Dodge/Dodge.obj");
+    //FManagerOBJ::CreateStaticMesh("Assets/Dodge/Dodge.obj");
 
-    FManagerOBJ::CreateStaticMesh("Assets/SkySphere.obj");
-    AActor* SpawnedActor = SpawnActor<AActor>();
-    USkySphereComponent* skySphere = SpawnedActor->AddComponent<USkySphereComponent>();
-    skySphere->SetStaticMesh(FManagerOBJ::GetStaticMesh(L"SkySphere.obj"));
-    skySphere->GetStaticMesh()->GetMaterials()[0]->Material->SetDiffuse(FVector((float)32/255, (float)171/255, (float)191/255));
+    //FManagerOBJ::CreateStaticMesh("Assets/SkySphere.obj");
+    //AActor* SpawnedActor = SpawnActor<AActor>();
+    //USkySphereComponent* skySphere = SpawnedActor->AddComponent<USkySphereComponent>();
+    //skySphere->SetStaticMesh(FManagerOBJ::GetStaticMesh(L"SkySphere.obj"));
+    //skySphere->GetStaticMesh()->GetMaterials()[0]->Material->SetDiffuse(FVector((float)32/255, (float)171/255, (float)191/255));
+
+
 }
 
 void UWorld::CreateBaseObject()
@@ -40,7 +42,7 @@ void UWorld::CreateBaseObject()
 
     if (LocalGizmo == nullptr)
     {
-        LocalGizmo = FObjectFactory::ConstructObject<UTransformGizmo>();
+        //LocalGizmo = FObjectFactory::ConstructObject<UTransformGizmo>();
     }
 }
 
@@ -76,7 +78,7 @@ void UWorld::Tick(float DeltaTime)
 {
 	camera->TickComponent(DeltaTime);
 	EditorPlayer->Tick(DeltaTime);
-	LocalGizmo->Tick(DeltaTime);
+	//LocalGizmo->Tick(DeltaTime);
 
     // SpawnActor()에 의해 Actor가 생성된 경우, 여기서 BeginPlay 호출
     for (AActor* Actor : PendingBeginPlayActors)
